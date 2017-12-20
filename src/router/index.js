72042832eb686ce44b023eb06a291ab3eb288e09
LiYueLong <1393926171@qq.com>
2017-12-20 15:02:26 +0800

    设置页面标题

--- src/router/index.js
+++ src/router/index.js
@@ -3,38 +3,64 @@ import Router from 'vue-router'
 
 Vue.use(Router);
 
-export default new Router({
+let router = new Router({
     routes: [
         {
             path: '/',
             name: 'index',
+            meta: {
+                title: '大埔河长制'
+            },
             component: () => import('@/components/index')
         },
         {
             path: '/map-info',
             name: 'mapInfo',
+            meta: {
+                title: '地图信息'
+            },
             component: () => import('@/components/mapInfo')
         },
         {
             path: '/river-list',
             name: 'riverList',
+            meta: {
+                title: '河湖名录'
+            },
             component: () => import('@/components/riverList')
         },
         {
             path: '/my-report',
             name: 'myReport',
+            meta: {
+                title: '我的爆料'
+            },
             component: () => import('@/components/myReport')
         },
         {
             path: '/article-list',
             name: 'articleList',
+            meta: {
+                title: '列表信息'
+            },
             component: () => import('@/components/articleList')
         },
         {
             path: '/article-detail/:id',
             name: 'articleDetail',
             props: true,
+            meta: {
+                title: '详细信息'
+            },
             component: () => import('@/components/articleDetail')
         }
     ]
 });
+
+// 设置页面标题
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.title}`;
+    next();
+});
+
+export default router;
